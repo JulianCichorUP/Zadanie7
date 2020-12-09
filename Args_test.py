b1e6856d136e46b87247ec0c6237958d70cbff74
JulianCichorUP <73111644+JulianCichorUP@users.noreply.github.com>
2020-12-09 14:39:55 +0100

    Przepisanie zgodnie ze standardami PEP8

--- Args_test.py
+++ Args_test.py
@@ -1,12 +1,13 @@
-def person_print(name,last_name,age,*others):
-    formatted_data = 'Imię: {}, nazwisko: {}, wiek: {}'.format(name,last_name,age)
+def person_print(name, last_name, age, *others):
+    formatted_data = 'Imię: {}, nazwisko: {}, wiek: {}' \
+                     .format(name, last_name, age)
     others_str = ' '
     for arg in others:
         others_str += arg + ' '
     print(formatted_data + others_str)
 
-person_print('Julian','Cichor',21,"pesel: 12345678912", "zawod: student")
+person_print('Julian', 'Cichor', 21, "pesel: 12345678912", "zawod: student")
 
 """
-Nie można, ponieważ argument args/kwargs musi być na końcu deklaracji funkcji. 
+Nie można, ponieważ argument args/kwargs musi być na końcu deklaracji funkcji.
 """
